77ce00848f819bd4ac28c4207f58d1fbbbce0de8
dongchul <sah3122@naver.com>
2019-09-27 17:10:18 +0900

    project init

---
+++ src/main/java/me/study/HelloServlet.java
@@ -0,0 +1,36 @@
+package me.study;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by dongchul on 2019-09-27.
+ */
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
+        resp.getWriter().println("<html>");
+        resp.getWriter().println("<head>");
+        resp.getWriter().println("<body>");
+        resp.getWriter().println("<h1>Hello Servlet</h1>");
+        resp.getWriter().println("</body>");
+        resp.getWriter().println("</head>");
+        resp.getWriter().println("</html>");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+}
