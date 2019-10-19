a72aca9d66c7623f86d7e6908bc951f9ae221da9
dongchul <sah3122@naver.com>
2019-10-19 16:46:34 +0900

    ADD Spring IoC Container

--- src/main/java/me/study/HelloServlet.java
+++ src/main/java/me/study/HelloServlet.java
@@ -1,5 +1,8 @@
 package me.study;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -20,13 +23,19 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doGet");
+        ApplicationContext context = (ApplicationContext) getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+        HelloSevice helloSevice = context.getBean(HelloSevice.class);
         resp.getWriter().println("<html>");
         resp.getWriter().println("<head>");
         resp.getWriter().println("<body>");
-        resp.getWriter().println("<h1>Hello Servlet</h1>");
+        resp.getWriter().println("<h1>Hello "+ helloSevice.getName() +"</h1>");
         resp.getWriter().println("</body>");
         resp.getWriter().println("</head>");
         resp.getWriter().println("</html>");
+    }
+
+    private Object getName() {
+        return getServletContext().getAttribute("name");
     }
 
     @Override
---
+++ src/main/java/me/study/HelloSevice.java
@@ -0,0 +1,11 @@
+package me.study;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloSevice {
+
+    public String getName() {
+        return "dong";
+    }
+}
