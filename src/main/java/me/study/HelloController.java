3478ed413d7d6c107956fffa331ca96b5279461d
dongchul <sah3122@naver.com>
2019-10-22 00:35:17 +0900

    ADD Dispatcher Servlet part 1

--- src/main/java/me/study/HelloController.java
+++ src/main/java/me/study/HelloController.java
@@ -1,17 +1,24 @@
 package me.study;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @Autowired
     HelloSevice helloSevice;
 
     @GetMapping("/hello")
-    public String Hello() {
+    public @ResponseBody String Hello() {
         return "Hello " + helloSevice.getName();
+    }
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "/WEB-INF/sample.jsp";
     }
 }
---
+++ src/main/webapp/WEB-INF/simple.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 이동철
+  Date: 2019-10-22
+  Time: 오전 12:29
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+Hello simple mvc
+</body>
+</html>
